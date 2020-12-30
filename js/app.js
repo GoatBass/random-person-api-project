ec6e9df420930c513724a9c2097887c995af0c81
Edu <edugacv@gmail.com>
2020-12-30 09:57:04 +0100

    Add comments on file

--- js/app.js
+++ js/app.js
@@ -1,5 +1,8 @@
 //Tu codigo aqui
+
+//selecting button where the event will act 
 let btn = document.querySelector('#btn')
+//Selecting all the siplays where the value will go
 let displayPhoto = document.querySelector('#photo')
 let displayFirstName = document.querySelector('#first')
 let displayLastName = document.querySelector('#last')
@@ -7,13 +10,17 @@ let displayCountry = document.querySelector('#country')
 let displayPhone = document.querySelector('#phone')
 let displayMail = document.querySelector('#email')
 
+//create de add event listener for the button selected
 btn.addEventListener('click', updatePerson)
 
 function updatePerson(){
+    //activate the api listener
     let xhttp = new XMLHttpRequest()
     xhttp.onreadystatechange = function(){
         if(this.readyState == 4 && this.status == 200){
             let persons = JSON.parse(this.responseText)
+
+            //function that makes the display changes
             displayEverything(persons)
         }
     }
